2e8535d69789500a4a51240e209f7edd70157744
rishu8925 <rishu.upadhyay_cs23@gla.ac.in>
2026-04-24 08:50:17 +0530

    Day-3

---
+++ q8BuyandSell2.java
@@ -0,0 +1,22 @@
+public class q8BuyandSell2 {
+
+    public static int maxProfit(int[] prices) {
+        int profit = 0;
+
+        // Sum all positive adjacent differences
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        int result = maxProfit(prices);
+        System.out.println("Maximum Profit: " + result);
+    }
+}
